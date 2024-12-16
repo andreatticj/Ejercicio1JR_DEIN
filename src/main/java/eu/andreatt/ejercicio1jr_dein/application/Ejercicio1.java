46ee2ce6a8c7dab697cae11830b5588d0e81155f
andreatticj <andreatticj@gmail.com>
2024-12-16 11:33:51 +0100

    ADD main Ejercicio1

---
+++ src/main/java/eu/andreatt/ejercicio1jr_dein/application/Ejercicio1.java
@@ -0,0 +1,55 @@
+package eu.andreatt.ejercicio1jr_dein.application;
+
+import eu.andreatt.ejercicio1jr_dein.bbdd.ConexionBD;
+import javafx.scene.control.Alert;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.util.JRLoader;
+import net.sf.jasperreports.view.JasperViewer;
+
+
+import java.io.InputStream;
+import java.util.HashMap;
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+
+        // comenzamos con el lanzamiento del informe
+        try {
+            // abrimos la base de datos
+            ConexionBD con = new ConexionBD();
+            //podemos crear un conjunto de parámetros si quisieramos pasárselo al informe
+            HashMap<String, Object> parameters = new HashMap<String, Object>();
+
+            InputStream reportStream = getClass().getResourceAsStream("/eu/andreatt/ejercicio1jr_dein/jasper/ejercicio1.jasper");
+            if (reportStream == null) {
+                System.out.println("El archivo no esta ahí");
+            }else {
+                System.out.println("El archivo se ha encontrado");
+            }
+            JasperReport report = (JasperReport) JRLoader.loadObject(reportStream);
+
+            //Atentos a la ruta del Jasper y a cómo enlazamos el archivo compilado por JasperReports
+            // JasperReport report = (JasperReport) JRLoader.loadObject(new File("src/main/resources/es/israeldelamo/jasperconmaven/reports/Cherry.jasper"));
+            JasperPrint jprint = JasperFillManager.fillReport(report, parameters, con.getConexion());
+            //Preparamos un visor, no intentaremos usar el salvar a PDF; para eso el SO ya nos da las impresoras A PDF
+            JasperViewer viewer = new JasperViewer(jprint, false);
+            //lanzamos la visión
+            viewer.setVisible(true);
+        } catch (Exception e) {
+            //como estamos en JavaFX tratamos la alerta aquí y comentamos el stack de consola.
+            // en desarrollo sería conveniente tenerlo descomentado
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText(null);
+            alert.setTitle("ERROR");
+            alert.setContentText("Ha ocurrido un error al abrir el informe. Pida ayuda en el foro");
+            alert.showAndWait();
+            //e.printStackTrace();
+        }
+
+    }
+
+
+}
